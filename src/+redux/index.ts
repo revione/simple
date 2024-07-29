659e82f412ec50455a21f579f8e1fdc613f7b19d
revione <revione@outlook.com>
2024-07-29 22:34:24 +0200

    update redux use

--- src/+redux/index.ts
+++ src/+redux/index.ts
@@ -5,9 +5,42 @@ import {
   useSelector as useReduxSelector,
 } from "react-redux"
 
-import { store } from "./configureStore"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 
-// store
+import reducer from "+redux/reducer"
+
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, combineReducers(reducer))
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
 
 export type State = ReturnType<typeof store.getState>
 
