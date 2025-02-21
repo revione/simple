59c5c14fef27cc153fbdf94bf5305bfa78757b44
revione <revione@outlook.com>
2025-02-21 16:19:46 +0100

    remove a logs error

--- src/sockets/buyer/events/error.ts
+++ src/sockets/buyer/events/error.ts
@@ -1,7 +1,5 @@
 import { state } from "+local"
 
 export const error = (event: Event) => {
-  if (state.logs.show_error_logs) {
-    console.log(":: socket buyer has an error : ", { event })
-  }
+  console.log(":: socket buyer has an error : ", { event })
 }
